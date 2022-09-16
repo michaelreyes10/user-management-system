85004c9581b03b5bc6ce030a5c8806ad494f3060
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-09-16 13:21:59 -0400

    engine bug fixing

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+//dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3005
+
+//parse middleware
+app.use(bodyParser.urlencoded({ extended: false}))
+
+//parse app/json
+app.use(bodyParser.json());
+
+//static files
+app.use(express.static('public'));
+
+//template
+app.engine('hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+//ROUTE
+app.get('', (req, res) => {
+    res.render('home');
+});
+
+
+
+//listening
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
