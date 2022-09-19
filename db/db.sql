a7608c62e28bd361ca6780a1da73be3267faf4ad
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-09-19 12:09:05 -0400

    added db files and dummy data

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS user_management_system;
+CREATE DATABASE IF NOT EXISTS user_management_system;
+USE user_management_system;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users ( `first_name`, `last_name`, `email`, `phone`, `comments`)
+VALUES
+('Michael', 'Reyes', 'mike@gmail.com', '0123456789', 'Comment successfully posted'
+);
